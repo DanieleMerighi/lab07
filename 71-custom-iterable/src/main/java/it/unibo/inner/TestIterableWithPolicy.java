cd76f128201c9a2d3a36f61609e75b158b5112f6
DanieleMerighi <daniele.merighi2@studio.unibo.it>
2023-11-10 11:25:33 +0100

    Completato metodo getIterableWithPolicy

--- 71-custom-iterable/src/main/java/it/unibo/inner/TestIterableWithPolicy.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/TestIterableWithPolicy.java
@@ -2,6 +2,8 @@ package it.unibo.inner;
 
 import it.unibo.inner.api.IterableWithPolicy;
 import it.unibo.inner.api.Predicate;
+import it.unibo.inner.impl.IterablePlainImpl;
+import it.unibo.inner.impl.IterableWithPolicyImpl;
 import it.unibo.inner.test.api.Product;
 import it.unibo.inner.test.impl.ProductImpl;
 
@@ -15,11 +17,11 @@ public class TestIterableWithPolicy {
     private TestIterableWithPolicy() {}
 
     private static <T> IterableWithPolicy<T> getIterableWithPolicy(T[] elements, Predicate<T> filter) {
-        return null; // TODO: return the implementation of IterableWithPolicy
+        return new IterableWithPolicyImpl<>(elements, filter);
     }
 
     private static <T> IterableWithPolicy<T> getIterableWithPolicy(T[] elements) {
-        return null; // TODO: return the implementation of IterableWithPolicy
+        return new IterableWithPolicyImpl<>(elements);
     }
 
     public static void main(String[] args) {
